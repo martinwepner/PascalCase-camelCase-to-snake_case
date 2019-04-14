133514fc3f5205e44b455e3dd4bf77ab21d6916e
Martin Wepner <martin.wepner@icloud.com>
2019-04-14 19:48:58 +0200

    add: multi cursor mode support

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,4 +4,9 @@ All notable changes to the "mssql-postgres-type-converter" extension will be doc
 Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
 
 ## [Unreleased]
+
+## [0.1.0]
 - Initial release
+
+## [0.2.0] - 2019-04-14
+- add: support for multi cursor mode
--- src/extension.ts
+++ src/extension.ts
@@ -6,7 +6,6 @@ export function activate(context: vscode.ExtensionContext) {
 
 	let disposable = vscode.commands.registerCommand('pascalCamel2Snake', () => {
 
-
 		// get active TextEditor, to change the text later on
 		let editor = vscode.window.activeTextEditor;
 		if (!editor) {
@@ -21,32 +20,34 @@ export function activate(context: vscode.ExtensionContext) {
 			if (!editor) {
 				return; // No open text editor
 			}
-			let selection = editor.selection;
-			let text = editor.document.getText(selection);
 
-			if (!text || text == null)
-				return;
+			for (const selection of editor.selections) {
+				let text = editor.document.getText(selection);
 
-			// No WhiteSpaces Allowed
-			if (text.match(/\s/g)) {
-				vscode.window.showErrorMessage(`Cant't parse '${text}', no Whitespaces allowed`);
-				return
+				if (!text || text == null)
+					return;
+
+				// No WhiteSpaces Allowed
+				if (text.match(/\s/g)) {
+					vscode.window.showErrorMessage(`Cant't parse '${text}', no Whitespaces allowed`);
+					return
+				}
+
+				// Check if two capital Letters other than ID are next to each other, in that case no auto convertion possible!
+				if (!text.match(/^(((?![A-Z][A-Z]).)|ID)*$/g)) {
+					vscode.window.showErrorMessage(`Cant't parse '${text}', because there are 2 or more capital letters - other than 'ID' - next to each other`);
+					return;
+				}
+
+				// Get the Pascal Case Parts of the original String
+				const PascalCaseParts = text.match(/((?:^[a-z]|ID|[A-Z]+)[^A-Z]*)/g)
+				if (!PascalCaseParts)
+					return;
+
+				// lowercase everything and join it with _underscores_
+				const postgresFormatString = PascalCaseParts.join('_').toLowerCase();
+				editBuilder.replace(selection, postgresFormatString);
 			}
-
-			// Check if two capital Letters other than ID are next to each other, in that case no auto convertion possible!
-			if (!text.match(/^(((?![A-Z][A-Z]).)|ID)*$/g)) {
-				vscode.window.showErrorMessage(`Cant't parse '${text}', because there are 2 or more capital letters - other than 'ID' - next to each other`);
-				return;
-			}
-
-			// Get the Pascal Case Parts of the original String
-			const PascalCaseParts = text.match(/((?:^[a-z]|ID|[A-Z]+)[^A-Z]*)/g)
-			if (!PascalCaseParts)
-				return;
-
-			// lowercase everything and join it with _underscores_
-			const postgresFormatString = PascalCaseParts.join('_').toLowerCase();
-			editBuilder.replace(selection, postgresFormatString);
 		});
 	});
 
